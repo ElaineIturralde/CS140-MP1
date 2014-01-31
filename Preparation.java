95173690a250444349aec3a940e915069ffa19a3
Elalala <elaineiturralde326@gmail.com>
2014-01-31 18:42:47 +0800

    Update Preparation.java

--- Preparation.java
+++ Preparation.java
@@ -1,23 +1,25 @@
 public class Preparation{
 	
+	//tentative layout for preparation
+	
 	int count = 0;
 	
-	public void cookInStove(){
+	public void prepareForStove(){
 		synchronized(this){
 			notify();
 			count++;
 		}
 	}
 	
-	public void prepareForStove(){
+	public void cookInStove(){
 		synchronized(this){
 			if(count > 0){
 				try{ 
-					System.out.println ("Waiting for the food to cook in the stove :D");
+					System.out.println ("Waiting for other food to be prepared");
 					this.wait();
 				}
 				catch (InterruptedException e) {}
-				System.out.println ("Preparations can be done!");
+				System.out.println ("Can cook in stove!");
 			}
 			count = 0;
 		}
