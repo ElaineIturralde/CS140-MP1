baf742da3f84e5682cdf54817c248a53abba7901
Elalala <elaineiturralde326@gmail.com>
2014-01-31 18:20:48 +0800

    Update Preparation.java

--- Preparation.java
+++ Preparation.java
@@ -1,3 +1,25 @@
 public class Preparation{
+	
+	int count = 0;
+	
+	public void cookInStove(){
+		synchronized(this){
+			notify();
+			count++;
+		}
+	}
+	
+	public void prepareForStove(){
+		synchronized(this){
+			if(count > 0){
+				try{ 
+					System.out.println ("Waiting for the food to cook in the stove :D");
+					this.wait();
+				}
+				catch (InterruptedException e) {}
+				System.out.println ("Preparations are complete!");
+			}
+		}
+	}
 
 }
