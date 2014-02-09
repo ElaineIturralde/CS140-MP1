c36ee15855f14dbe0caf82db4695d3ffce430467
Elalala <elaineiturralde326@gmail.com>
2014-02-09 15:23:25 +0800

    Update Stove.java

--- Stove.java
+++ Stove.java
@@ -7,4 +7,18 @@ public class Stove{
   public void add(Action a){
     this.stove.add(a);
   }
+  
+  public void startCoocking(){
+    
+    //Evaluate all tasks in need of assistants
+    for(int i = 0; i < stove.size(); i++){
+      
+      Action a = new Action();
+      a = stove.get(i);
+      for(int j = 0; j < a.getTime(); j++){
+        
+        //Iterate until the time expires
+      }
+    }
+  }
 }
