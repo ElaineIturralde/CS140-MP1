58527bee61ec88e2312308e50aed8fa262c272c1
Elalala <elaineiturralde326@gmail.com>
2014-02-09 15:18:36 +0800

    Update Assistants.java

--- Assistants.java
+++ Assistants.java
@@ -5,6 +5,22 @@ public class Assitants{
   public Assistants(){}
   
   public void add(Action a){
+    
     this.stove.add(a);
+  }
+  
+  public void startPreparing(){
+    
+    //Evaluate all tasks in need of assistants
+    //Prob: hindi ko talaga maisip kung pano kapag maraming assistants ><
+    for(int i = 0; i < ast.size(); i++){
+      
+      Action a = new Action();
+      a = ast.get(i);
+      for(int j = 0; j < a.getTime(); j++){
+        
+        //Iterate until the time expires
+      }
+    }
   }
 }
