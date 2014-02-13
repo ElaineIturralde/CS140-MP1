76e939416d056c2961820e2d096c091f402574ee
Elalala <elaineiturralde326@gmail.com>
2014-02-13 13:23:27 +0800

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,32 +1,49 @@
 public class Action{
 	
+	private String recipe;
 	private String name;
 	private int time;
 	private boolean isDone = false;
 	private boolean isCookingStep;
+	private boolean isExecuting = false;
 
-	public Action(){}
-	
-	public Action(String name, int time){
+	public Action(String recipe, String name, int time){
+		this.recipe = recipe;
 		this.name = name;
 		this.time = time;
 		isCookingStep = (name.toLowerCase().compareTo("cook") == 0);
 	}
-
-	public void switchDone(){
-		this.isDone = !(this.isDone);
+	
+	public void execute(){
+		this.isExecuting = true;
 	}
-
-	public void switchDone(boolean isDone){
-		this.isDone = isDone;
+	
+	public void setTime(int time){
+		this.time = time;
+	}
+	
+	public void setIsDone(){
+		this.isDone = true;
+	}
+	
+	public String getRecipe(){
+		return this.recipe;
 	}
 
 	public String getName(){
 		return this.name;
 	}
+	
+	public boolean getIsCookingStep(){
+		return this.isCookingStep;
+	}
 
 	public int getTime(){
 		return this.time;
+	}
+	
+	public boolean getIsExecuting(){
+		return this.isExecuting;
 	}
 
 	public boolean isDone(){
@@ -34,7 +51,7 @@ public class Action{
 	}
 
 	public String toString(){
-		return "Action: " + this.name + " Time: " + this.time + " isDone: " + this.isDone;
+		return "Recipe: " + this.recipe + "Action: " + this.name + " Time: " + this.time + " isDone: " + this.isDone;
 	}
 
 }
