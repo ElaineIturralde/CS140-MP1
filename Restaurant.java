7972bc08f807dc74fac3c52c764fbb8ab196a91e
Elalala <elaineiturralde326@gmail.com>
2014-02-13 13:24:35 +0800

    Update Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -1,174 +1,193 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Restaurant{
-
-	public static void main(String args[]){
-		
-		RecipeBook rb = new RecipeBook();
-		ArrayList<Task> tasklist = new ArrayList<Task>();
-
-		try{
-			
-			// Reads the tasklist.txt.
-			BufferedReader br = new BufferedReader(new FileReader("tasklist.txt"));
-			
-			while(br.ready()){
-				
-				// Extracts the recipes from the tasklist.txt that are to be cooked.
-				String read = br.readLine();
-				
-				Task t = new Task();
-				t.setName(read.substring(0, splitString(read)));
-				t.setStartTime(Integer.parseInt(read.substring(splitString(read) + 1, read.length())));
-				tasklist.add(t);
-				
-				if(rb.getSize() == 0 || !(rb.searchRecipe(t.getName()))){
-					
-					// Reads the recipe.txt.
-					BufferedReader br2 = new BufferedReader(new FileReader(t.getName() + ".txt"));
-					
-					// Extracts the name and priority of the recipe.
-					String read2 = br2.readLine();
-					String name = read2.substring(0, splitString(read2));
-					int priority = Integer.parseInt(read2.substring(splitString(read2) + 1, read2.length()));
-					ArrayList<Action> actions = new ArrayList<Action>();
-					
-					// Extracts the actions needed to be done in preparing the particular recipe.
-					while(br2.ready()){
-						String read3 = br2.readLine();
-						Action a = new Action(read3.substring(0, splitString(read3)), Integer.parseInt(read3.substring(splitString(read3) + 1, read3.length())));
-						actions.add(a);
-					}
-					Recipe r = new Recipe(name, priority, actions);
-					rb.addRecipe(r);
-				}
-			}
-			// Just prints the recipes in the recipe book.
-			/**
-			for(int ac = 0; ac < rb.getSize(); ac++){
-				Recipe r = rb.getRecipe(ac);
-				System.out.println(r.getName() + " " +  r.getPriority());
-				ArrayList<Action> a = r.getActions();
-				for(int bc = 0; bc < a.size(); bc++){
-					System.out.println(a.get(bc).getName() + " " + a.get(bc).getTime());
-				}
-			}
-			**/
-		}catch(FileNotFoundException e){
-			e.printStackTrace();
-			System.exit(1);
-		}catch(IOException e){
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
-		//I made 2 Kinds of Evaluate methods. Pacheck na lang
-		evaluate(tasklist, rb);
-	}
-	
-	//Evaluates each task if the food is to be cooked in the stove or to be handled by the assistants
-	//Evaluate 1
-	public static void evaluate(ArrayList<Task> tasklist, RecipeBook rb){
-		
-		ArrayList<Action> cookActions = new ArrayList<Action>();
-		ArrayList<Action> prepActions = new ArrayList<Action>();
-		
-		//@total_time para lang yan sa pagenter ng cooking actions. So kailangan pa yan iincrement.
-		//Iniisip ko rin na magset na lang tayo ng predefined time tas break na lang natin kapag
-		//tapos na lahat ng actions
-		int total_time = 0;
-		
-		//Iterate to traverse all the tasks in the tasklist
-		for(int i = 0; i < tasklist.size(); i++){
-			
-			Recipe r = new Recipe();
-			total_time += tasklist.get(i).getStartTime();
-			
-			//Finds the recipe to be evaluated
-			for(int j = 0; j < rb.size(); j++){
-			
-				r = rb.get(j);
-				if(r.getName().equals(tasklist.getName())){
-					break;	
-				}
-				j++;
-			}
-			
-			//Breaks down the recipe - stove and assistants
-			for(int j = 0; j < r.getActions().size(); j++){
-				
-				Action a = new Action();
-				a = r.getActions().get(j);
-				if(a.isCookingStep){
-					
-					cookActions.add(a);
-				}
-				else{
-					
-					prepActions.add(a);
-				}
-			}
-		}
-		
-		for(int i = 0; i < total_time; i++){
-			
-			//Handles time of cooking and preparing. 
-			//But, since all cooking actions and preparing actions are together, 
-			//there should be some way to know which recipe the action is for
-			//and some way para malaman na tapos na yung cooking para makapagprepare
-			//or the other way around.
-		}
-	}
-	
-	//Evaluates each task if the food is to be cooked in the stove or to be handled by the assistants
-	//Evaluate2
-	public static void evaluate(ArrayList<Task> tasklist, RecipeBook rb){
-		
-		Stove st = new Stove();
-		Assistants ast = new Assistants();
-		
-		//Iterate to traverse all the tasks in the tasklist
-		for(int i = 0; i < tasklist.size(); i++){
-			
-			Recipe r = new Recipe();
-			int j = 0;
-			
-			//Finds the recipe to be evaluated
-			while(j < rb.size()){
-			
-				r = rb.get(j);
-				if(r.getName().equals(tasklist.getName())){
-					break;	
-				}
-				j++;
-			}
-			
-			//Breaks down the recipe - stove and assistants
-			for(j = 0; j < r.getActions().size(); j++){
-				
-				Action a = new Action();
-				a = r.getActions().get(j);
-				if(a.isCookingStep){
-					
-					st.add(a);
-				}
-				else{
-					
-					ast.add(a);
-				}
-			}
-		}
-	}
-	
-	// Created own splitString function. Basically it, it returns the position of the character before the occurence of the first digit.
-	// That position serves as the place where the string would be splitted.
-	public static int splitString(String s){
-		
-		for(int ac = 0; ac < s.length(); ac++){
-			if(Character.isDigit(s.charAt(ac))) return ac - 1;
-		}
-		return -1;
-	}
-
+ 
+        public static void main(String args[]){
+               
+                RecipeBook rb = new RecipeBook();
+                LinkedList<Task> tasklist = new LinkedList<Task>();
+ 
+                try{
+                       
+                        // Reads the tasklist.txt.
+                        BufferedReader br = new BufferedReader(new FileReader("tasklist.txt"));
+                       
+                        while(br.ready()){
+                               
+                                // Extracts the recipes from the tasklist.txt that are to be cooked.
+                                String read = br.readLine();
+                               
+                                Task t = new Task();
+                                t.setName(read.substring(0, splitString(read)));
+                                t.setStartTime(Integer.parseInt(read.substring(splitString(read) + 1, read.length())));
+                                tasklist.add(t);
+                               
+                                if(rb.getSize() == 0 || !(rb.searchRecipe(t.getName()))){
+                                       
+                                        // Reads the recipe.txt.
+                                        BufferedReader br2 = new BufferedReader(new FileReader(t.getName() + ".txt"));
+                                       
+                                        // Extracts the name and priority of the recipe.
+                                        String read2 = br2.readLine();
+                                        String name = read2.substring(0, splitString(read2));
+                                        int priority = Integer.parseInt(read2.substring(splitString(read2) + 1, read2.length()));
+                                        LinkedList<Action> actions = new LinkedList<Action>();
+                                       
+                                        // Extracts the actions needed to be done in preparing the particular recipe.
+                                        while(br2.ready()){
+                                                String read3 = br2.readLine();
+                                                Action a = new Action(read.substring(0, splitString(read)), read3.substring(0, splitString(read3)), Integer.parseInt(read3.substring(splitString(read3) + 1, read3.length())));
+                                                actions.add(a);
+                                        }
+                                        Recipe r = new Recipe(name, priority, actions);
+                                        rb.addRecipe(r);
+                                }
+                        }
+                        // Just prints the recipes in the recipe book.
+                        /**
+                        for(int ac = 0; ac < rb.getSize(); ac++){
+                                Recipe r = rb.getRecipe(ac);
+                                System.out.println(r.getName() + " " +  r.getPriority());
+                                ArrayList<Action> a = r.getActions();
+                                for(int bc = 0; bc < a.size(); bc++){
+                                        System.out.println(a.get(bc).getName() + " " + a.get(bc).getTime());
+                                }
+                        }
+                        **/
+                       
+                        // Prints the tasks in the tasklist.
+                        /**
+                        for(int ac = 0; ac < tasklist.size(); ac++){
+                                Task t = tasklist.get(ac);
+                                System.out.println(t.getName() + " " + t.getStartTime());
+                        }
+                        **/
+                       
+                        // Execution Process
+                        LinkedList<Recipe> orders = new LinkedList<Recipe>();
+                        LinkedList<Action> cookActions = new LinkedList<Action>();
+                        LinkedList<Action> prepActions = new LinkedList<Action>();
+                        PrintStream ps = new PrintStream("output.txt");
+                        int time = 1;
+                        int time_of_highest_priority = -1, index_of_highest_priority = -1, highest_priority = 0;
+						String recipe_of_highest_priority = "", tentrecipe = "";
+						int z = 0;
+                       
+                        while(true){
+                               
+                                Task t;
+                                Recipe r;
+                                System.out.println ("Time " + time);
+                                if(tasklist.size() != 0 && time == tasklist.get(0).getStartTime()){
+                                        t = tasklist.remove();
+                                        r = rb.getRecipe(rb.getIndex(t.getName()));
+                                        orders.add(r);
+                                }
+                               
+                                for(int ac = 0; ac < orders.size(); ac++){
+                                        if(!orders.get(ac).getActions().isEmpty()){
+                                        	Action a = orders.get(ac).getActions().get(0);
+                                        	if(a.getName().equalsIgnoreCase("prep") && !a.getIsExecuting()){
+                                                a.execute();
+                                                prepActions.add(a);
+                                        	}
+                                        }
+                                }
+                               
+                                for(int ac = 0; ac < prepActions.size(); ac++){
+                                        System.out.println(prepActions.get(ac).getRecipe() + " " + prepActions.get(ac).getTime() + " " + time);
+                                }
+                                
+                                if(time_of_highest_priority < 1 && !orders.isEmpty()){
+                                	highest_priority = 0;
+                                	
+                                	/*//checking
+                                	for(int ac = 0; ac < orders.size(); ac++){
+                                		System.out.println ("order " + orders.get(ac).getName());
+                                	}*/
+                                	
+                                	for(int ac = 0; ac < orders.size(); ac++){
+                                		if(!orders.get(ac).getActions().isEmpty() && orders.get(ac).getActions().get(0).getName().equalsIgnoreCase("cook")){
+                                			cookActions.add(orders.get(ac).getActions().get(0));
+                                		}
+                                	}
+                                	if(!cookActions.isEmpty()){
+                                		boolean contains = false;
+                                		for(int ac = 0; ac < orders.size(); ac++){
+                                			for(int bc = 0; bc < cookActions.size(); bc++){
+                                				if(orders.get(ac).getNewPriority() > highest_priority && orders.get(ac).getName().equals(cookActions.get(bc).getRecipe())){
+                                					highest_priority = orders.get(ac).getNewPriority();
+                                					recipe_of_highest_priority = orders.get(ac).getName();
+                                					time_of_highest_priority = orders.get(ac).getActions().get(0).getTime();
+                                					index_of_highest_priority = ac;
+                                					contains = true;
+                                				}
+                                			}
+                                		}
+                                		if(contains){
+                                			orders.get(index_of_highest_priority).getActions().remove(0);
+                                			System.out.println ("cooking: " + recipe_of_highest_priority + " " + highest_priority + " " + time_of_highest_priority);
+                                		}
+                                	}
+                                }
+                                else{
+                                	System.out.println ("cooking: " + recipe_of_highest_priority + " " + highest_priority + " " + time_of_highest_priority);
+                                }
+                                time_of_highest_priority--;
+                               
+                                //Execution Preparation Process
+                                for(int ac = 0; ac < prepActions.size(); ac++){
+                                       
+                                        Action a = prepActions.get(ac);
+                                        a.setTime(a.getTime() - 1);
+                                       
+                                        if(a.getTime() == 0){
+                                                a.setIsDone();
+                                                prepActions.remove(ac);
+                                                ac--;
+                                                for(int bc = 0; bc < orders.size(); bc++){
+                                                       
+                                                        if(a.getName().equals(orders.get(bc).getName())){
+                                                                orders.get(bc).getActions().remove();
+                                                        }
+                                                       
+                                                        if(orders.get(bc).getActions().size() == 0){
+                                                                orders.remove();
+                                                        }
+                                                }
+                                        }
+                                }
+                               
+                                //Execution of Cooking Process
+                               
+                               
+                                time++;
+                               
+                                if(tasklist.size() == 0 && prepActions.size() == 0 && cookActions.size() == 0){
+                                        break;
+                                }
+                                z++;
+                               
+                        }
+                       
+                }catch(FileNotFoundException e){
+                        e.printStackTrace();
+                        System.exit(1);
+                }catch(IOException e){
+                        e.printStackTrace();
+                        System.exit(1);
+                }
+        }
+       
+        // Created own splitString function. Basically it, it returns the position of the character before the occurence of the first digit.
+        // That position serves as the place where the string would be splitted.
+        public static int splitString(String s){
+               
+                for(int ac = 0; ac < s.length(); ac++){
+                        if(Character.isDigit(s.charAt(ac))) return ac - 1;
+                }
+                return -1;
+        }
+ 
 }
